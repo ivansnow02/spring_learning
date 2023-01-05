29a634f87444d4cc50a5a462b7fadd6798e79928
ivansnow02 <ivansnow02@gmail.com>
2023-01-05 14:40:42 +0800

    ApplicationContext入门

---
+++ src/main/java/com/is/test/ApplicationContextTest.java
@@ -0,0 +1,13 @@
+package com.is.test;
+
+import com.is.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextTest {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        System.out.println(userService);
+    }
+}

295428a0331f58da0679079aa4df26f350a62abe
ivansnow02 <ivansnow02@gmail.com>
2023-01-05 14:26:39 +0800

    BeanFactory入门

--- src/main/java/com/is/service/impl/UserServiceImpl.java
+++ src/main/java/com/is/service/impl/UserServiceImpl.java
@@ -4,8 +4,10 @@ import com.is.dao.UserDao;
 import com.is.service.UserService;
 
 public class UserServiceImpl implements UserService {
+    private UserDao userDao;
     //BeanFactory去调用该方法 从容器中获得userDao设置到此处
     public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
         System.out.println("BeanFactory去调用该方法获得userDao设置到此处:" + userDao);
     }
 }

2770f706a315139baaa51e666c3fb0fccedb0ff0
ivansnow02 <ivansnow02@gmail.com>
2023-01-05 14:18:47 +0800

    BeanFactory入门

---
+++ src/main/java/com/is/dao/impl/UserDaoImpl.java
@@ -0,0 +1,6 @@
+package com.is.dao.impl;
+
+import com.is.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+}
---
+++ src/main/java/com/is/service/UserService.java
@@ -0,0 +1,4 @@
+package com.is.service;
+
+public interface UserService {
+}

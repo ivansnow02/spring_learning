c3bcf63746c0b84faca7b023b4a9278d56587baa
ivansnow02 <ivansnow02@gmail.com>
2023-01-09 15:11:22 +0800

    XmlSpring配置非自定义Bean

--- src/main/java/com/is/test/ApplicationContextTest.java
+++ src/main/java/com/is/test/ApplicationContextTest.java
@@ -1,11 +1,22 @@
 package com.is.test;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.is.service.UserService;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ApplicationContextTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 //        UserService userService = (UserService) applicationContext.getBean("userService");
 //        System.out.println(userService);
@@ -13,7 +24,44 @@ public class ApplicationContextTest {
 //        System.out.println(userDao1);
 //        Object userDao2 = applicationContext.getBean("userDao2");
 //        System.out.println(userDao2);
-        UserService userService = (UserService) applicationContext.getBean("userService");
-        userService.show();
+//        UserService userService = (UserService) applicationContext.getBean("userService");
+//        userService.show();
+
+
+
+//        DruidDataSource dataSource = new DruidDataSource();
+//        dataSource.setDriverClassName();
+//        dataSource.setUrl();
+//        dataSource.setUsername();
+//        dataSource.setPassword();
+//        Object dataSource = applicationContext.getBean("dataSource");
+//        System.out.println(dataSource);
+//        Object connection = applicationContext.getBean("connection");
+//        System.out.println(connection);
+
+
+
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        Date date = simpleDateFormat.parse("2023-8-27 12:00:00");
+
+
+
+//        Object date = applicationContext.getBean("date");
+//        System.out.println(date);
+
+//==================================================================
+//        //静态工厂方法
+//        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
+//        //无参构造实例化
+//        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+//        //实例工厂方法
+//        SqlSessionFactory sqlSessionFactory = builder.build(in);
+
+        SqlSessionFactory sqlSessionFactory =(SqlSessionFactory) applicationContext.getBean("sqlSessionFactory");
+        System.out.println(sqlSessionFactory);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        System.out.println(sqlSession);
+
+
     }
 }
